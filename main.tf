a22a6848ef95b294d2615a1c56de16db56ea6b45
Matias Vera <matias@bajacloud.io>
2022-11-15 08:27:17 -0500

    s3fix

--- main.tf
+++ main.tf
@@ -28,6 +28,21 @@ resource "aws_s3_bucket" "data01" {
   }
 }
 
+resource "aws_s3_bucket" "data02" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data02"
+  region        = "us-west-2"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data02"
+    Environment = local.resource_prefix.value
+  }
+}
+
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   region        = "us-west-2"
