8ed2cced285885a285babd81880a6ad95c50501f
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2023-06-25 22:46:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "data" {
   # bucket is public
-  # bucket is not encrypted
+  # bucket is not encrypted 
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
